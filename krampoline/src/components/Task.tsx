74da1ed23f1d56a5819fabe360a0dcc248cb2b74
Seoa Kim <seoa521@gmail.com>
2023-10-27 14:26:29 +0900

    [feat] 오늘/진행 중 투두 리스트 필터 기능 추가

--- krampoline/src/components/Task.tsx
+++ krampoline/src/components/Task.tsx
@@ -3,7 +3,7 @@ import Checkbox from "@mui/material/Checkbox";
 import { PiTrashLight, PiPencilLight } from "react-icons/pi";
 
 function Task({ todo, onUpdate, onDelete }) {
-  const { id, title, status } = todo;
+  const { id, content, status } = todo;
 
   const handleChange = (e) => {
     onUpdate({ ...todo, status: e.target.checked ? "completed" : "active" });
@@ -19,12 +19,17 @@ function Task({ todo, onUpdate, onDelete }) {
           id={id}
           checked={status === "completed"}
           onChange={handleChange}
-          defaultChecked
           color="secondary"
           className="cursor-pointer"
         />
-        <label htmlFor={id} className="flex justify-left items-center">
-          {title}
+        <label
+          htmlFor={id}
+          className="flex justify-left items-center"
+          style={{
+            textDecoration: status === "completed" ? "line-through" : "none",
+          }}
+        >
+          {content}
         </label>
       </div>
       <span className="flex justify-right items-center">
--- krampoline/src/pages/Todo.tsx
+++ krampoline/src/pages/Todo.tsx
@@ -1,12 +1,74 @@
-import React from "react";
-import TodoList from "../components/TodoList";
+import React, { useState, useEffect } from "react";
+import Task from "../components/Task";
 
 function Todo() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch(`data/todos.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data);
+      });
+
+    return () => {
+      console.log("cleanup");
+    };
+  }, []);
+
+  // useEffect(() => {
+  //   localStrage.setItem("todos", JSON.stringify(todos));
+  // }, [todos]);
+
+  const handleUpdate = (updated) => {
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  };
+
+  const handleDelete = (deleted) => {
+    setTodos(todos.filter((t) => t.id !== deleted.id));
+  };
+
   return (
-    <div>
-      <TodoList />
-    </div>
+    <>
+      <div className="flex flex-col m-4">
+        <button className="w-28 h-10 shrink-0 bg-amber-400 border-black border-2 rounded-full flex items-center font-bold justify-center my-2">
+          오늘
+        </button>
+        <ul>
+          {todos.map((item) => (
+            <Task
+              key={item.id}
+              todo={item}
+              onUpdate={handleUpdate}
+              onDelete={handleDelete}
+            />
+          ))}
+        </ul>
+      </div>
+      <div className="flex flex-col m-4">
+        <button className="w-28 h-10 shrink-0 bg-amber-400 border-black border-2 rounded-full flex items-center font-bold justify-center my-2">
+          진행 중
+        </button>
+        <ul>
+          {todos
+            .filter((item) => item.status === "active")
+            .map((item) => (
+              <Task
+                key={item.id}
+                todo={item}
+                onUpdate={handleUpdate}
+                onDelete={handleDelete}
+              />
+            ))}
+        </ul>
+      </div>
+    </>
   );
 }
+
+// function readTodos() {
+//   const todos = localStrage.getItem("todos");
+//   return todos ? JSON.parse(todos) : [];
+// }
 
 export default Todo;
