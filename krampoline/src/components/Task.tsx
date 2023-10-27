b11d0a4e6c28f273be32750e64c27ca6c7617dec
Seoa Kim <seoa521@gmail.com>
2023-10-27 16:17:28 +0900

    [fix] CSS, 빌드 에러 수정, 필터 기능 삭제

--- krampoline/src/components/Task.tsx
+++ krampoline/src/components/Task.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import { ChangeEvent } from "react";
 import Checkbox from "@mui/material/Checkbox";
 import { PiTrashLight, PiPencilLight } from "react-icons/pi";
 
-function Task({ todo, onUpdate, onDelete }) {
+type TaskProps = {
+  todo: {
+    id: string;
+    content: string;
+    status: string;
+  };
+  onUpdate: (todo: { id: string; content: string; status: string }) => void;
+  onDelete: (todo: { id: string; content: string; status: string }) => void;
+};
+
+function Task({ todo, onUpdate, onDelete }: TaskProps) {
   const { id, content, status } = todo;
 
-  const handleChange = (e) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     onUpdate({ ...todo, status: e.target.checked ? "completed" : "active" });
   };
 
@@ -15,7 +25,6 @@ function Task({ todo, onUpdate, onDelete }) {
     <li className="flex justify-between items-center w-30 h-12 shrink-0 border-black border-2 rounded-[15px] shadow-standard bg-white my-4">
       <div className="flex justify-left items-center">
         <Checkbox
-          type="checkbox"
           id={id}
           checked={status === "completed"}
           onChange={handleChange}
--- krampoline/src/pages/Todo.tsx
+++ krampoline/src/pages/Todo.tsx
@@ -1,8 +1,14 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Task from "../components/Task";
 
+type TodoItem = {
+  id: string;
+  content: string;
+  status: string;
+};
+
 function Todo() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<TodoItem[]>([]);
 
   useEffect(() => {
     fetch(`data/todos.json`)
@@ -10,29 +16,25 @@ function Todo() {
       .then((data) => {
         setTodos(data);
       });
-
-    return () => {
-      console.log("cleanup");
-    };
   }, []);
 
   // useEffect(() => {
   //   localStrage.setItem("todos", JSON.stringify(todos));
   // }, [todos]);
 
-  const handleUpdate = (updated) => {
+  const handleUpdate = (updated: TodoItem) => {
     setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
   };
 
-  const handleDelete = (deleted) => {
+  const handleDelete = (deleted: TodoItem) => {
     setTodos(todos.filter((t) => t.id !== deleted.id));
   };
 
   return (
-    <>
+    <div className="pb-16">
       <div className="flex flex-col m-4">
-        <button className="w-28 h-10 shrink-0 bg-amber-400 border-black border-2 rounded-full flex items-center font-bold justify-center my-2">
-          오늘
+        <button className="w-32 h-10 shrink-0 bg-amber-400 border-black border-2 rounded-full flex items-center font-bold justify-center my-2">
+          오늘 할 일
         </button>
         <ul>
           {todos.map((item) => (
@@ -46,23 +48,21 @@ function Todo() {
         </ul>
       </div>
       <div className="flex flex-col m-4">
-        <button className="w-28 h-10 shrink-0 bg-amber-400 border-black border-2 rounded-full flex items-center font-bold justify-center my-2">
-          진행 중
+        <button className="w-32 h-10 shrink-0 bg-amber-500 border-black border-2 rounded-full flex items-center font-bold justify-center my-2">
+          진행 중 할 일
         </button>
         <ul>
-          {todos
-            .filter((item) => item.status === "active")
-            .map((item) => (
-              <Task
-                key={item.id}
-                todo={item}
-                onUpdate={handleUpdate}
-                onDelete={handleDelete}
-              />
-            ))}
+          {todos.map((item) => (
+            <Task
+              key={item.id}
+              todo={item}
+              onUpdate={handleUpdate}
+              onDelete={handleDelete}
+            />
+          ))}
         </ul>
       </div>
-    </>
+    </div>
   );
 }
 
