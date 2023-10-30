0d568e9deaf3836245cc4e7486ac656fa30ab712
Seoa Kim <128363466+records-sa@users.noreply.github.com>
2023-10-30 15:41:04 +0900

    투두 리스트 UI 구현 (#4)
    * [feat] 투두 리스트 UI 구현
    
    * [feat] 오늘/진행 중 투두 리스트 필터 기능 추가
    
    * [fix] CSS, 빌드 에러 수정, 필터 기능 삭제
    
    * [refactor] 주석 제거 및 Tasks로 컴포넌트 분리
    
    ---------
    
    Co-authored-by: confidential-nt <youyoy689@gmail.com>

---
+++ krampoline/src/components/Task.tsx
@@ -0,0 +1,52 @@
+import { ChangeEvent } from "react";
+import Checkbox from "@mui/material/Checkbox";
+import { PiTrashLight, PiPencilLight } from "react-icons/pi";
+import { TodoItem } from "../pages/Todo";
+
+type Props = {
+  todo: TodoItem;
+  onUpdate: (todo: TodoItem) => void;
+  onDelete: (todo: TodoItem) => void;
+};
+
+function Task({ todo, onUpdate, onDelete }: Props) {
+  const { id, content, status } = todo;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onUpdate({ ...todo, status: e.target.checked ? "completed" : "active" });
+  };
+
+  const handleDelete = () => onDelete(todo);
+
+  return (
+    <li className="flex justify-between items-center w-30 h-12 shrink-0 border-black border-2 rounded-[15px] shadow-standard bg-white my-4">
+      <div className="flex justify-left items-center">
+        <Checkbox
+          id={id}
+          checked={status === "completed"}
+          onChange={handleChange}
+          color="secondary"
+          className="cursor-pointer"
+        />
+        <label
+          htmlFor={id}
+          className="flex justify-left items-center"
+          style={{
+            textDecoration: status === "completed" ? "line-through" : "none",
+          }}
+        >
+          {content}
+        </label>
+      </div>
+      <span className="flex justify-right items-center">
+        <PiPencilLight className="w-6 h-6 shrink-0 mx-1 cursor-pointer" />
+        <PiTrashLight
+          onClick={handleDelete}
+          className="w-6 h-6 shrink-0 mx-1 cursor-pointer"
+        />
+      </span>
+    </li>
+  );
+}
+
+export default Task;
--- krampoline/src/pages/Todo.tsx
+++ krampoline/src/pages/Todo.tsx
@@ -1,3 +1,47 @@
-export default function Todo() {
-  return <div>여기는 todo 페이지</div>;
+import { useState, useEffect } from "react";
+import Tasks from "../components/Tasks";
+
+export type TodoItem = {
+  id: string;
+  content: string;
+  status: string;
+};
+
+function Todo() {
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+
+  useEffect(() => {
+    fetch(`data/todos.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data);
+      });
+  }, []);
+
+  const handleUpdate = (updated: TodoItem) => {
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  };
+
+  const handleDelete = (deleted: TodoItem) => {
+    setTodos(todos.filter((t) => t.id !== deleted.id));
+  };
+
+  return (
+    <div className="pb-16">
+      <div className="flex flex-col m-4">
+        <label className="w-32 h-10 shrink-0 bg-amber-400 border-black border-2 rounded-full flex items-center font-bold justify-center my-2">
+          오늘 할 일
+        </label>
+        <Tasks todos={todos} onDelete={handleDelete} onUpdate={handleUpdate} />
+      </div>
+      <div className="flex flex-col m-4">
+        <label className="w-32 h-10 shrink-0 bg-amber-500 border-black border-2 rounded-full flex items-center font-bold justify-center my-2">
+          진행 중 할 일
+        </label>
+        <Tasks todos={todos} onDelete={handleDelete} onUpdate={handleUpdate} />
+      </div>
+    </div>
+  );
 }
+
+export default Todo;
