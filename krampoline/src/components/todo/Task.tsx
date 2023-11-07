0dc471716f8c1969e86540f07a0ddc2b9567ad27
Seoa Kim <seoa521@gmail.com>
2023-11-07 15:23:35 +0900

    [fix] return 조건문 else 삭제

--- krampoline/src/components/todo/Task.tsx
+++ krampoline/src/components/todo/Task.tsx
@@ -57,40 +57,39 @@ function Task({ todo, onUpdate, onDelete }: Props) {
         </span>
       </li>
     );
-  } else {
-    return (
-      <li className="flex justify-between items-center w-30 h-12 shrink-0 border-black border-2 rounded-[15px] shadow-standard bg-white my-4">
-        <div className="flex justify-left items-center">
-          <Checkbox
-            id={id}
-            checked={status === "completed"}
-            onChange={handleChange}
-            color="secondary"
-            className="cursor-pointer"
-          />
-          <label
-            htmlFor={id}
-            className="flex justify-left items-center"
-            style={{
-              textDecoration: status === "completed" ? "line-through" : "none",
-            }}
-          >
-            {content}
-          </label>
-        </div>
-        <span className="flex justify-right items-center">
-          <MdOutlineCreate
-            onClick={() => setIsEditing(true)}
-            className="w-6 h-6 shrink-0 mx-1 cursor-pointer"
-          />
-          <HiOutlineTrash
-            onClick={handleDelete}
-            className="w-6 h-6 shrink-0 mx-1 cursor-pointer"
-          />
-        </span>
-      </li>
-    );
   }
+  return (
+    <li className="flex justify-between items-center w-30 h-12 shrink-0 border-black border-2 rounded-[15px] shadow-standard bg-white my-4">
+      <div className="flex justify-left items-center">
+        <Checkbox
+          id={id}
+          checked={status === "completed"}
+          onChange={handleChange}
+          color="secondary"
+          className="cursor-pointer"
+        />
+        <label
+          htmlFor={id}
+          className="flex justify-left items-center"
+          style={{
+            textDecoration: status === "completed" ? "line-through" : "none",
+          }}
+        >
+          {content}
+        </label>
+      </div>
+      <span className="flex justify-right items-center">
+        <MdOutlineCreate
+          onClick={() => setIsEditing(true)}
+          className="w-6 h-6 shrink-0 mx-1 cursor-pointer"
+        />
+        <HiOutlineTrash
+          onClick={handleDelete}
+          className="w-6 h-6 shrink-0 mx-1 cursor-pointer"
+        />
+      </span>
+    </li>
+  );
 }
 
 export default Task;
