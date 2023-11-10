2a1326fa45aeb38c55b5d8c7e3e7469b136afc83
confidential-nt <youyoy689@gmail.com>
2023-11-10 18:15:25 +0900

    [fix] 존재하지 않는 css 파일 때문에 에러나는 것 수정

--- krampoline/src/components/CalendarDialog.tsx
+++ krampoline/src/components/CalendarDialog.tsx
@@ -1,177 +1,193 @@
-import { useState } from 'react';
-import dayjs from 'dayjs';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { MobileTimePicker } from '@mui/x-date-pickers/MobileTimePicker';
-import './timePicker.css'
+import { useState } from "react";
+import dayjs from "dayjs";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { MobileTimePicker } from "@mui/x-date-pickers/MobileTimePicker";
 import {
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogContentText,
-    DialogActions,
-    Button,
-    Slide,
-    TextField,
-    ThemeProvider,
-    createTheme
-} from '@mui/material';
-import React from 'react';
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+  Slide,
+  TextField,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+import React from "react";
 
 type Event = {
-    id: number;
-    title: string;
-    status: string;
-    start: Date;
-    end: Date;
+  id: number;
+  title: string;
+  status: string;
+  start: Date;
+  end: Date;
 };
 
 type CalendarDialogProps = {
-    value: {
-        eventTitle: string;
-        setEventTitle: React.Dispatch<React.SetStateAction<string>>;
-        selectedRange: {
-            start: Date;
-            end: Date;
-        } | null;
-        setSelectedRange: React.Dispatch<React.SetStateAction<{ start: Date; end: Date } | null>>;
-        isDialogOpen: boolean;
-        setIsDialogOpen: React.Dispatch<React.SetStateAction<boolean>>;
-        events: Event[];
-        setNewEvent: React.Dispatch<React.SetStateAction<Event | null>>;
-    };
+  value: {
+    eventTitle: string;
+    setEventTitle: React.Dispatch<React.SetStateAction<string>>;
+    selectedRange: {
+      start: Date;
+      end: Date;
+    } | null;
+    setSelectedRange: React.Dispatch<
+      React.SetStateAction<{ start: Date; end: Date } | null>
+    >;
+    isDialogOpen: boolean;
+    setIsDialogOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    events: Event[];
+    setNewEvent: React.Dispatch<React.SetStateAction<Event | null>>;
+  };
 };
 
 const theme = createTheme({
-    components: {
-        MuiTextField: {
-            styleOverrides: {
-                root: {
-                    '& .MuiInputBase-input': {
-                        display: 'flex',
-                        height: '2rem',
-                        border: '2px solid #000',
-                        borderRadius: '15px',
-                        boxShadow: '4px 4px 0 0',
-                        marginTop: '0.8rem',
-                        paddingRight: '10rem',
-                        paddingLeft: '0.8rem'
-                    },
-                },
-            },
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          "& .MuiInputBase-input": {
+            display: "flex",
+            height: "2rem",
+            border: "2px solid #000",
+            borderRadius: "15px",
+            boxShadow: "4px 4px 0 0",
+            marginTop: "0.8rem",
+            paddingRight: "10rem",
+            paddingLeft: "0.8rem",
+          },
         },
+      },
     },
+  },
 });
 
-
 const CalendarDialog: React.FC<CalendarDialogProps> = ({ value }) => {
-    const {
-        eventTitle,
-        setEventTitle,
-        selectedRange,
-        setSelectedRange,
-        isDialogOpen,
-        setIsDialogOpen,
-        events,
-        setNewEvent
-    } = value;
+  const {
+    eventTitle,
+    setEventTitle,
+    selectedRange,
+    setSelectedRange,
+    isDialogOpen,
+    setIsDialogOpen,
+    events,
+    setNewEvent,
+  } = value;
 
-    const [startTime, setStartTime] = useState<Date | null>(dayjs().toDate());
-    const [endTime, setEndTime] = useState<Date | null>(dayjs().add(1, 'hour').toDate());
+  const [startTime, setStartTime] = useState<Date | null>(dayjs().toDate());
+  const [endTime, setEndTime] = useState<Date | null>(
+    dayjs().add(1, "hour").toDate()
+  );
 
-    const handleCloseDialog = () => {
-        setIsDialogOpen(false);
-    };
+  const handleCloseDialog = () => {
+    setIsDialogOpen(false);
+  };
 
-    const handleEventSave = () => {
-        if (eventTitle && selectedRange && startTime && endTime) {
-            const startWithTime = dayjs(selectedRange.start)
-                .hour(dayjs(startTime).hour())
-                .minute(dayjs(startTime).minute())
-                .toDate();
+  const handleEventSave = () => {
+    if (eventTitle && selectedRange && startTime && endTime) {
+      const startWithTime = dayjs(selectedRange.start)
+        .hour(dayjs(startTime).hour())
+        .minute(dayjs(startTime).minute())
+        .toDate();
 
-            const endWithTime = dayjs(selectedRange.end)
-                .hour(dayjs(endTime).hour())
-                .minute(dayjs(endTime).minute())
-                .toDate();
+      const endWithTime = dayjs(selectedRange.end)
+        .hour(dayjs(endTime).hour())
+        .minute(dayjs(endTime).minute())
+        .toDate();
 
-            const event: Event = {
-                id: events.length + 1,
-                title: eventTitle,
-                status: '진행중',
-                start: startWithTime,
-                end: endWithTime,
-            };
+      const event: Event = {
+        id: events.length + 1,
+        title: eventTitle,
+        status: "진행중",
+        start: startWithTime,
+        end: endWithTime,
+      };
 
-            setNewEvent(event);
-            setEventTitle('');
-            setSelectedRange(null);
-            handleCloseDialog();
-        }
-    };
+      setNewEvent(event);
+      setEventTitle("");
+      setSelectedRange(null);
+      handleCloseDialog();
+    }
+  };
 
+  return (
+    <Dialog
+      open={isDialogOpen}
+      TransitionComponent={Slide}
+      onClose={handleCloseDialog}
+      className="w-200 h-300"
+    >
+      <DialogTitle className="border-b-2">Todo 입력</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          <ThemeProvider theme={theme}>
+            <div className="flex flex-col md:flex-row mt-2">
+              <h1 className="w-24 h-8 bg-amber-400 border-black border-2 rounded-full flex items-center font-bold justify-center my-2 mr-5 mt-4">
+                Todo
+              </h1>
+              <TextField
+                variant="standard"
+                InputProps={{
+                  disableUnderline: true,
+                }}
+                value={eventTitle}
+                onChange={(e) => setEventTitle(e.target.value)}
+              />
+            </div>
+          </ThemeProvider>
+          <div className="flex flex-col md:flex-row">
+            <h1 className="w-24 h-8 bg-amber-400 border-black border-2 rounded-full flex items-center font-bold justify-center my-2 mr-3 mt-8">
+              날짜
+            </h1>
+            <p className="flex justify-between items-center sm:w-50 md:w-[23rem] lg:w-[23rem] xl:w-[23rem] h-12 border-black border-2 rounded-[15px] shadow-standard bg-white my-4 pl-3 ml-2 mt-6">
+              {selectedRange?.start?.toLocaleDateString()} ~{" "}
+              {selectedRange?.end?.toLocaleDateString()
+                ? new Date(
+                    selectedRange.end.getTime() - 24 * 60 * 60 * 1000
+                  ).toLocaleDateString()
+                : ""}
+            </p>
+          </div>
+          <div className="flex flex-col md:flex-row">
+            <h1 className="w-24 h-8 bg-amber-400 border-black border-2 rounded-full flex items-center font-bold justify-center my-2 mr-5 mt-4">
+              시작 시간
+            </h1>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <MobileTimePicker
+                value={dayjs(startTime)}
+                onChange={(newStartTime) => {
+                  if (newStartTime) {
+                    setStartTime(newStartTime.toDate());
+                  }
+                }}
+              />
+            </LocalizationProvider>
+          </div>
+          <div className="flex flex-col md:flex-row">
+            <h1 className="w-24 h-8 bg-amber-400 border-black border-2 rounded-full flex items-center font-bold justify-center my-2 mr-5 mt-4">
+              마감 시간
+            </h1>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <MobileTimePicker
+                value={dayjs(endTime)}
+                onChange={(newEndTime) => {
+                  if (newEndTime) {
+                    setEndTime(newEndTime.toDate());
+                  }
+                }}
+              />
+            </LocalizationProvider>
+          </div>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions className="border-t-2">
+        <Button onClick={handleEventSave}>입력</Button>
+        <Button onClick={handleCloseDialog}>취소</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
 
-    return (
-        <Dialog open={isDialogOpen} TransitionComponent={Slide} onClose={handleCloseDialog} className="w-200 h-300">
-            <DialogTitle className="border-b-2">Todo 입력</DialogTitle>
-            <DialogContent>
-                <DialogContentText>
-                    <ThemeProvider theme={theme}>
-                        <div className="flex flex-col md:flex-row mt-2">
-                            <h1 className="w-24 h-8 bg-amber-400 border-black border-2 rounded-full flex items-center font-bold justify-center my-2 mr-5 mt-4">Todo</h1>
-                            <TextField
-                                variant="standard"
-                                InputProps={{
-                                    disableUnderline: true,
-                                }}
-                                value={eventTitle}
-                                onChange={(e) => setEventTitle(e.target.value)}
-                            />
-                        </div>
-                    </ThemeProvider>
-                    <div className="flex flex-col md:flex-row">
-                        <h1 className="w-24 h-8 bg-amber-400 border-black border-2 rounded-full flex items-center font-bold justify-center my-2 mr-3 mt-8">날짜</h1>
-                        <p className="flex justify-between items-center sm:w-50 md:w-[23rem] lg:w-[23rem] xl:w-[23rem] h-12 border-black border-2 rounded-[15px] shadow-standard bg-white my-4 pl-3 ml-2 mt-6">
-                            {selectedRange?.start?.toLocaleDateString()} ~ {selectedRange?.end?.toLocaleDateString() ? new Date(selectedRange.end.getTime() - 24 * 60 * 60 * 1000).toLocaleDateString() : ''}
-                        </p>
-                    </div>
-                    <div className="flex flex-col md:flex-row">
-                        <h1 className="w-24 h-8 bg-amber-400 border-black border-2 rounded-full flex items-center font-bold justify-center my-2 mr-5 mt-4">시작 시간</h1>
-                        <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <MobileTimePicker
-                                value={dayjs(startTime)}
-                                onChange={(newStartTime) => {
-                                    if (newStartTime) {
-                                        setStartTime(newStartTime.toDate());
-                                    }
-                                }}
-                            />
-                        </LocalizationProvider>
-                    </div>
-                    <div className="flex flex-col md:flex-row">
-                        <h1 className="w-24 h-8 bg-amber-400 border-black border-2 rounded-full flex items-center font-bold justify-center my-2 mr-5 mt-4">마감 시간</h1>
-                        <LocalizationProvider
-                            dateAdapter={AdapterDayjs}
-                        >
-                            <MobileTimePicker
-                                value={dayjs(endTime)}
-                                onChange={(newEndTime) => {
-                                    if (newEndTime) {
-                                        setEndTime(newEndTime.toDate());
-                                    }
-                                }}
-                            />
-
-                        </LocalizationProvider>
-                    </div>
-                </DialogContentText>
-            </DialogContent>
-            <DialogActions className="border-t-2">
-                <Button onClick={handleEventSave}>입력</Button>
-                <Button onClick={handleCloseDialog}>취소</Button>
-            </DialogActions>
-        </Dialog>
-    )
-}
-
-export default CalendarDialog
+export default CalendarDialog;
