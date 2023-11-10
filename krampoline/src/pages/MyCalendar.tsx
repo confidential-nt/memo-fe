e60b5a0f8d8ace09e2067032cd0e241c37728dea
did-jiwon <puding0712@naver.com>
2023-11-10 15:50:26 +0900

    [feat] 로컬 스토리지에 todo 저장

--- krampoline/src/pages/MyCalendar.tsx
+++ krampoline/src/pages/MyCalendar.tsx
@@ -6,80 +6,85 @@ import CalendarDialog from '../components/calendarDialog/CalendarDialog';
 
 const localizer = luxonLocalizer(DateTime);
 
+function readTodosFromLocalStorage() {
+  const eventsDataString = localStorage.getItem("eventsData");
+  const eventsData = eventsDataString ? JSON.parse(eventsDataString) : [];
+
+  const modifiedEventsData: Event[] = eventsData.map((data: Event) => ({
+    ...data,
+    start: new Date(data.start),
+    end: new Date(data.end),
+  }));
+
+  return modifiedEventsData;
+}
+
 type Event = {
   id: number;
-  content: string;
+  title: string;
   status: string,
-  startDate: Date;
-  endDate: Date;
+  start: Date;
+  end: Date;
 };
 
 const MyCalendar = () => {
-  const [events, setEvents] = useState<Event[]>([]);
+  const [events, setEvents] = useState<Event[]>(() => readTodosFromLocalStorage());
   const [newEvent, setNewEvent] = useState<Event | null>(null);
   const [eventTitle, setEventTitle] = useState('');
   const [selectedRange, setSelectedRange] = useState<{ start: Date; end: Date } | null>(null);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   useEffect(() => {
-    // JSON 파일 로드 및 파싱
-    fetch(`data/todos.json`) // JSON 파일 경로에 따라 수정
-      .then((res) => res.json())
-      .then((data) => {
-        // JSON 데이터를 Event 데이터로 변환
-        const eventList = data.map((item: Event) => ({
-          id: item.id,
-          title: item.content,
-          status: item.status,
-          start: new Date(item.startDate),
-          end: new Date(item.endDate),
-        }));
-        setEvents(eventList);
+    if (newEvent) {
+      setEvents((prevEvents) => {
+        const updatedEvents = [...prevEvents, newEvent];
+        localStorage.setItem('eventsData', JSON.stringify(updatedEvents));
+        return updatedEvents;
       });
-  }, [events]);
+      setNewEvent(null);
+    }
+  }, [newEvent]);
 
   const handleOpenDialog = () => {
-      setIsDialogOpen(true);
-    };
+    setIsDialogOpen(true);
+  };
 
   const handleSelectSlot = ({ start, end }: { start: Date; end: Date }) => {
     setSelectedRange({ start, end });
     handleOpenDialog();
-
-    if (newEvent) {
-      setEvents([...events, newEvent]);
-      setNewEvent(null);
-    }
   };
-
 
   return (
     <div className="h-[30rem]">
-      <Calendar
-        localizer={localizer}
-        events={events}
-        // startAccessor="start"
-        // endAccessor="end"
-        views={{
-          month: true,
-        }}
-        formats={{
-          monthHeaderFormat: 'yyyy-MM-dd',
-        }}
-        selectable
-        onSelectSlot={handleSelectSlot}
-        popup
-      />
-      <CalendarDialog value={{ 
-        eventTitle,
-        setEventTitle,
-        selectedRange,
-        setSelectedRange,
-        isDialogOpen,
-        setIsDialogOpen,
-        events,
-        setEvents,
-      }}/>
+      {
+        <>
+          <Calendar
+            localizer={localizer}
+            events={events}
+            startAccessor="start"
+            endAccessor="end"
+            views={{
+              month: true,
+            }}
+            formats={{
+              monthHeaderFormat: 'yyyy-MM-dd',
+            }}
+            selectable
+            onSelectSlot={handleSelectSlot}
+            popup
+          />
+          <CalendarDialog value={{
+            eventTitle,
+            setEventTitle,
+            selectedRange,
+            setSelectedRange,
+            isDialogOpen,
+            setIsDialogOpen,
+            events,
+            setNewEvent
+          }} />
+        </>
+      }
     </div>
   );
 };
