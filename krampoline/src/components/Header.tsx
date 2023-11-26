0eb2de1e9e4665ef85abfb9111be30e3667d4945
confidential-nt <youyoy689@gmail.com>
2023-11-26 23:58:12 +0900

    [feat] 비로그인 사용자한테는 퀵 인풋 및 버튼 disabled 되도록 구현

--- krampoline/src/components/Header.tsx
+++ krampoline/src/components/Header.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import { useLocation } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
 
 export default function Header() {
   const { pathname } = useLocation();
+  const { user } = useAuthContext();
 
   useEffect(() => {}, [pathname]);
 
@@ -12,10 +14,14 @@ export default function Header() {
       <form className="flex items-center mb-2">
         <input
           type="text"
-          className="w-full h-[2rem] block border-2 border-black rounded-[15px] shadow-standard mr-5 p-1"
-          placeholder="ex)#todo 내일까지 과제하기 or #memo 사과는 맛있다."
+          className="w-full h-[2rem] block border-2 border-black rounded-[15px] shadow-standard mr-5 p-1 disabled:cursor-not-allowed"
+          placeholder="ex)#todo 내일까지 과제하기(로그인 후 사용가능)"
+          disabled={!user}
         />
-        <button className="rounded-full bg-white w-[2.8rem] h-[2.5rem] flex justify-center items-center border-2 border-black shadow-standard active:shadow-none active:translate-x-1 active:translate-y-1">
+        <button
+          disabled={!user}
+          className="rounded-full bg-white w-[2.8rem] h-[2.5rem] flex justify-center items-center border-2 border-black shadow-standard active:shadow-none active:translate-x-1 active:translate-y-1 disabled:active:shadow-standard disabled:active:translate-x-0 disabled:active:translate-y-0 disabled:cursor-not-allowed disabled:bg-transparent"
+        >
           <AiOutlinePlus className="text-3xl" />
         </button>
       </form>
