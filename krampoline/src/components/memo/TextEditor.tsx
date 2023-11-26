bb3bb1a7615293e16a85817ba622dbe6f0d7f358
confidential-nt <youyoy689@gmail.com>
2023-11-26 23:22:22 +0900

    [feat] 유저 로그인 시 텍스트 작성 내용 서버에 가도록 구현

--- krampoline/src/components/memo/TextEditor.tsx
+++ krampoline/src/components/memo/TextEditor.tsx
@@ -3,7 +3,10 @@ import { Editor as TinyMCEEditor } from "tinymce";
 import { useCallback, useRef, useState } from "react";
 import debounce from "lodash.debounce";
 import { Memo } from "../../types/Memo.types";
-import { saveMemoContent } from "../../service/database/api";
+import { saveMemoContent as saveMemoContentInLocal } from "../../service/database/api";
+import { useAuthContext } from "../../context/AuthContext";
+import { AuthenticatedUser } from "../../types/Auth.types";
+import useMemoActionsInServer from "../../hooks/useMemoActionsInServer";
 
 const editorInitConfig = {
   height: "85vh",
@@ -43,12 +46,23 @@ type Props = {
 
 export default function TextEditor({ memo }: Props) {
   const editorRef = useRef<TinyMCEEditor | null>(null);
+  const { user } = useAuthContext();
+  const { onSaveMemoContent } = useMemoActionsInServer(null);
   const [dirty, setDirty] = useState(false);
+
+  const saveMemoContent = (
+    memoId: string,
+    content: string,
+    user?: AuthenticatedUser | null
+  ) =>
+    !user
+      ? saveMemoContentInLocal(memoId, content)
+      : onSaveMemoContent(memoId, content);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedSave = useCallback(
     debounce(async (value: string, memo: Memo) => {
-      await saveMemoContent(memo.id, value);
+      await saveMemoContent(memo.id, value, user);
       setDirty(false);
     }, 1000),
     []
