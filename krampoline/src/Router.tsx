d8ab7bc1a0ab3fbcdabcd53523c3ad10ef25a9c1
confidential-nt <youyoy689@gmail.com>
2023-11-26 18:09:27 +0900

    [fix] 프로덕션 환경에서 /today로 들어가면 제대로 페이지 못불러오는 에러 수정 시도 5

--- krampoline/src/Router.tsx
+++ krampoline/src/Router.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Todo from "./pages/Todo";
 import MyCalendar from "./pages/MyCalendar";
@@ -9,9 +9,8 @@ export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/" index element={<Todo />} />
-        {/* <Route path="/" element={<Navigate to="/today" replace />} /> */}
-        <Route path="/today" element={<Todo />} />
+        <Route path="/" element={<Navigate to="/today" replace />} />
+        <Route path="/today" index element={<Todo />} />
         <Route path="/openApi/kakao" element={<Login />}></Route>
         <Route path="/calendar" element={<MyCalendar />} />
         <Route path="/memo" element={<Memo />} />
