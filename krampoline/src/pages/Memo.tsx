d1ddf37168f3cac54edfa8ad72b04381d96d7e45
confidential-nt <youyoy689@gmail.com>
2024-01-30 02:22:55 +0900

    [refactor] 메모 컴포넌트에서 트리뷰 관련 컴포넌트 통째로 이동

--- krampoline/src/pages/Memo.tsx
+++ krampoline/src/pages/Memo.tsx
@@ -1,24 +1,17 @@
-import { Suspense, lazy, useEffect } from "react";
+import { useEffect } from "react";
 import { useUserContext } from "../context/UserContext";
 import { LuFolderTree } from "react-icons/lu";
 import { useAuthContext } from "../context/AuthContext";
 import useMemoStore from "../hooks/useMemoStore";
 import { initializeAppAfterFirstLogin } from "../utils/memo";
 import useIndexedDBMemoStore from "../hooks/useIndexedDBMemoStore";
-import TreeViewContextProvider from "../context/TreeViewContext";
-import TextEditorPlaceholder from "../components/placeholder/TextEditorPlaceholder";
-import TreeViewPlaceholder from "../components/placeholder/TreeViewPlaceholder";
-import useMemoes from "../hooks/useMemoes";
 import useDrawer from "../hooks/useDrawer";
-
-const TextEditor = lazy(() => import("../components/memo/TextEditor"));
-const TreeViewHOC = lazy(() => import("../components/memo/TreeViewHOC"));
+import TreeViewContainer from "../components/memo/TreeViewContainer";
 
 export default function Memo() {
   const { tempUserId } = useUserContext();
   const { user } = useAuthContext();
 
-  const { memo } = useMemoes();
   const { isDrawerOpened, toggleDrawer } = useDrawer();
 
   const { memoStore } = useIndexedDBMemoStore(tempUserId);
@@ -42,25 +35,7 @@ export default function Memo() {
       >
         <LuFolderTree className="text-white" />
       </button>
-      <TreeViewContextProvider>
-        <>
-          <Suspense fallback={<TreeViewPlaceholder className="md:hidden" />}>
-            <TreeViewHOC className="md:hidden" />
-          </Suspense>
-          <div className="md:grow">
-            <Suspense fallback={<TextEditorPlaceholder />}>
-              <TextEditor memo={memo} />
-            </Suspense>
-          </div>
-          <Suspense
-            fallback={
-              <TreeViewPlaceholder className="hidden md:block md:w-[300px] md:ml-3" />
-            }
-          >
-            <TreeViewHOC className="hidden md:block md:ml-3" />
-          </Suspense>
-        </>
-      </TreeViewContextProvider>
+      <TreeViewContainer />
     </section>
   );
 }
